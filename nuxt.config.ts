e266a1c47e0eaa91f6ab10197f4b0ad80ab65fbd
javonpeart <javonpeart@gmail.com>
2024-08-29 11:18:05 -0500

    configure nitro preset for Firebase deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,14 @@ export default defineNuxtConfig({
     '/about': { ssr: true },
   },
 
+  nitro: {
+    preset: 'firebase',
+    firebase: {
+      nodeVersion: '18',
+      gen: 1,
+      region: 'us-central1'
+    }
+  },
+
   compatibilityDate: '2024-08-29',
 })
